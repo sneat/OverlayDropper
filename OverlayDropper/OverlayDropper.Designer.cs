e52f94ac76e826a6b75a9d6a3a0019ab9b7d0547
Blair <blair@mcmillan.id.au>
2013-11-08 17:37:59 +1000

    Initial commit

---
+++ OverlayDropper/OverlayDropper.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace OverlayDropper
+{
+    partial class OverlayDropper
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.checkBoxEnabled = new System.Windows.Forms.CheckBox();
+            this.GameSelector = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // checkBoxEnabled
+            // 
+            this.checkBoxEnabled.AutoSize = true;
+            this.checkBoxEnabled.Location = new System.Drawing.Point(207, 12);
+            this.checkBoxEnabled.Name = "checkBoxEnabled";
+            this.checkBoxEnabled.Size = new System.Drawing.Size(65, 17);
+            this.checkBoxEnabled.TabIndex = 0;
+            this.checkBoxEnabled.Text = "Enabled";
+            this.checkBoxEnabled.UseVisualStyleBackColor = true;
+            this.checkBoxEnabled.CheckedChanged += new System.EventHandler(this.checkBoxEnabled_CheckedChanged);
+            // 
+            // GameSelector
+            // 
+            this.GameSelector.FormattingEnabled = true;
+            this.GameSelector.Items.AddRange(new object[] {
+            "",
+            "League of Legends",
+            "Starcraft 2"});
+            this.GameSelector.Location = new System.Drawing.Point(12, 29);
+            this.GameSelector.Name = "GameSelector";
+            this.GameSelector.Size = new System.Drawing.Size(121, 21);
+            this.GameSelector.TabIndex = 1;
+            this.GameSelector.SelectedIndexChanged += new System.EventHandler(this.GameSelector_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Game:";
+            // 
+            // OverlayDropper
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.GameSelector);
+            this.Controls.Add(this.checkBoxEnabled);
+            this.Name = "OverlayDropper";
+            this.Text = "Overlay Dropper by ACLPro";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckBox checkBoxEnabled;
+        private System.Windows.Forms.ComboBox GameSelector;
+        private System.Windows.Forms.Label label1;
+
+    }
+}
+
---
+++ OverlayDropper/OverlayDropper.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using MouseKeyboardActivityMonitor;
+using MouseKeyboardActivityMonitor.WinApi;
+using System.Runtime.InteropServices;
+namespace OverlayDropper
+{
+    public partial class OverlayDropper : Form
+    {
+        private Boolean IsCtrlDown;
+        private Boolean IsShiftDown;
+        private Boolean IsAltDown;
+        private String SelectedGame;
+        private KeyboardHookListener m_KeyboardHookManager;
+
+        [DllImport("user32.dll")]
+        static extern IntPtr GetForegroundWindow();
+
+        [DllImport("user32.dll")]
+        static extern int GetWindowText(IntPtr hWnd, StringBuilder text, int count);
+
+        public OverlayDropper()
+        {
+            InitializeComponent();
+            m_KeyboardHookManager = new KeyboardHookListener(new GlobalHooker());
+        }
+
+        #region Initialisation of events and application
+        private void checkBoxEnabled_CheckedChanged(object sender, EventArgs e)
+        {
+            m_KeyboardHookManager.Enabled = checkBoxEnabled.Checked;
+            if (checkBoxEnabled.Checked)
+            {
+                m_KeyboardHookManager.KeyDown += HookManager_KeyDown;
+                m_KeyboardHookManager.KeyUp += HookManager_KeyUp;
+            }
+            else
+            {
+                m_KeyboardHookManager.KeyDown -= HookManager_KeyDown;
+                m_KeyboardHookManager.KeyUp -= HookManager_KeyUp;
+            }
+        }
+        #endregion
+
+        #region Event handlers of particular events. They will be activated when an appropriate checkbox is checked.
+
+
+        private void GameSelector_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            SelectedGame = GameSelector.SelectedItem.ToString();
+        }
+        
+        private void HookManager_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode.ToString() == "LControlKey")
+            {
+                IsCtrlDown = true;
+            }
+            if (e.KeyCode.ToString() == "LShiftKey")
+            {
+                IsShiftDown = true;
+            }
+            if (e.KeyCode.ToString() == "LAltKey")
+            {
+                IsAltDown = true;
+            }
+
+            Log(string.Format("KeyDown \t\t {0}\n", e.KeyCode));
+        }
+
+        private void HookManager_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode.ToString() == "LControlKey")
+            {
+                IsCtrlDown = false;
+            }
+            if (e.KeyCode.ToString() == "LShiftKey")
+            {
+                IsShiftDown = false;
+            }
+            if (e.KeyCode.ToString() == "LAltKey")
+            {
+                IsAltDown = false;
+            }
+
+            if (SelectedGame == "League of Legends" && e.KeyCode.ToString() == "A")
+            {
+                ToggleLeagueOfLegendsOverlays();
+            }
+
+            if (SelectedGame == "Starcraft 2" && IsCtrlDown && e.KeyCode.ToString() == "W")
+            {
+                ToggleStarcraft2Overlays();
+            }
+
+            Log(string.Format("KeyUp  \t\t {0}\n", e.KeyCode));
+        }
+        #endregion
+
+        #region Game specific events
+        private void ToggleLeagueOfLegendsOverlays()
+        {
+            // TODO
+            Log("Toggling LoL\n");
+        }
+
+        private void ToggleStarcraft2Overlays()
+        {
+            // TODO
+            Log("Toggling SC2\n");
+        }
+        #endregion
+
+        private void Log(string text)
+        {
+            System.Console.WriteLine(string.Format("{0}, {1}", text, GetActiveWindowTitle()));
+        }
+
+        private string GetActiveWindowTitle()
+        {
+            const int nChars = 256;
+            IntPtr handle = IntPtr.Zero;
+            StringBuilder Buff = new StringBuilder(nChars);
+            handle = GetForegroundWindow();
+
+            if (GetWindowText(handle, Buff, nChars) > 0)
+            {
+                return Buff.ToString();
+            }
+            return null;
+        }
+    }
+}
