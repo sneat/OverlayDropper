da845beeeadf0cc879526086cbd83ecb691ecf9a
Blair <blair@mcmillan.id.au>
2013-11-09 13:55:40 +1000

    Adding ATEM DLL

--- OverlayDropper/OverlayDropper.Designer.cs
+++ OverlayDropper/OverlayDropper.Designer.cs
@@ -28,15 +28,26 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(OverlayDropper));
             this.checkBoxEnabled = new System.Windows.Forms.CheckBox();
             this.GameSelector = new System.Windows.Forms.ComboBox();
             this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.ATEMIpAddressTextBox = new System.Windows.Forms.TextBox();
+            this.ATEMConnectButton = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.HotKeyLabel = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.ProgLabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // checkBoxEnabled
             // 
             this.checkBoxEnabled.AutoSize = true;
-            this.checkBoxEnabled.Location = new System.Drawing.Point(207, 12);
+            this.checkBoxEnabled.Location = new System.Drawing.Point(204, 101);
             this.checkBoxEnabled.Name = "checkBoxEnabled";
             this.checkBoxEnabled.Size = new System.Drawing.Size(65, 17);
             this.checkBoxEnabled.TabIndex = 0;
@@ -46,12 +57,13 @@
             // 
             // GameSelector
             // 
+            this.GameSelector.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.GameSelector.FormattingEnabled = true;
             this.GameSelector.Items.AddRange(new object[] {
             "",
             "League of Legends",
             "Starcraft 2"});
-            this.GameSelector.Location = new System.Drawing.Point(12, 29);
+            this.GameSelector.Location = new System.Drawing.Point(53, 99);
             this.GameSelector.Name = "GameSelector";
             this.GameSelector.Size = new System.Drawing.Size(121, 21);
             this.GameSelector.TabIndex = 1;
@@ -60,22 +72,115 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 13);
+            this.label1.Location = new System.Drawing.Point(9, 102);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(38, 13);
             this.label1.TabIndex = 2;
             this.label1.Text = "Game:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(106, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "ATEM Software v4.2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(9, 33);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(61, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "IP Address:";
+            // 
+            // ATEMIpAddressTextBox
+            // 
+            this.ATEMIpAddressTextBox.Location = new System.Drawing.Point(76, 31);
+            this.ATEMIpAddressTextBox.Name = "ATEMIpAddressTextBox";
+            this.ATEMIpAddressTextBox.Size = new System.Drawing.Size(100, 20);
+            this.ATEMIpAddressTextBox.TabIndex = 5;
+            this.ATEMIpAddressTextBox.TextChanged += new System.EventHandler(this.ATEMIpAddressTextBox_TextChanged);
+            // 
+            // ATEMConnectButton
+            // 
+            this.ATEMConnectButton.Location = new System.Drawing.Point(197, 28);
+            this.ATEMConnectButton.Name = "ATEMConnectButton";
+            this.ATEMConnectButton.Size = new System.Drawing.Size(75, 23);
+            this.ATEMConnectButton.TabIndex = 6;
+            this.ATEMConnectButton.Text = "Connect";
+            this.ATEMConnectButton.UseVisualStyleBackColor = true;
+            this.ATEMConnectButton.Click += new System.EventHandler(this.ATEMConnectButton_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Location = new System.Drawing.Point(12, 61);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(260, 2);
+            this.groupBox1.TabIndex = 7;
+            this.groupBox1.TabStop = false;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(9, 77);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(79, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Hotkey Binding";
+            // 
+            // HotKeyLabel
+            // 
+            this.HotKeyLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.HotKeyLabel.Location = new System.Drawing.Point(12, 132);
+            this.HotKeyLabel.Name = "HotKeyLabel";
+            this.HotKeyLabel.Size = new System.Drawing.Size(257, 23);
+            this.HotKeyLabel.TabIndex = 10;
+            this.HotKeyLabel.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::OverlayDropper.Properties.Resources.ACLProLogo;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 158);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(260, 101);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 11;
+            this.pictureBox1.TabStop = false;
+            // 
+            // ProgLabel
+            // 
+            this.ProgLabel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.ProgLabel.Location = new System.Drawing.Point(121, 9);
+            this.ProgLabel.Name = "ProgLabel";
+            this.ProgLabel.Size = new System.Drawing.Size(151, 16);
+            this.ProgLabel.TabIndex = 12;
+            this.ProgLabel.Text = "Not Connected";
+            this.ProgLabel.TextAlign = System.Drawing.ContentAlignment.TopRight;
             // 
             // OverlayDropper
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.ProgLabel);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.HotKeyLabel);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.ATEMConnectButton);
+            this.Controls.Add(this.ATEMIpAddressTextBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.GameSelector);
             this.Controls.Add(this.checkBoxEnabled);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "OverlayDropper";
             this.Text = "Overlay Dropper by ACLPro";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -86,6 +191,15 @@
         private System.Windows.Forms.CheckBox checkBoxEnabled;
         private System.Windows.Forms.ComboBox GameSelector;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox ATEMIpAddressTextBox;
+        private System.Windows.Forms.Button ATEMConnectButton;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label HotKeyLabel;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label ProgLabel;
 
     }
 }
--- OverlayDropper/OverlayDropper.cs
+++ OverlayDropper/OverlayDropper.cs
@@ -9,15 +9,37 @@ using System.Windows.Forms;
 using MouseKeyboardActivityMonitor;
 using MouseKeyboardActivityMonitor.WinApi;
 using System.Runtime.InteropServices;
+using BMDSwitcherAPI;
+using SwitcherPanelCSharp;
+
 namespace OverlayDropper
 {
     public partial class OverlayDropper : Form
     {
+        #region Keybook hooks
         private Boolean IsCtrlDown;
         private Boolean IsShiftDown;
         private Boolean IsAltDown;
         private String SelectedGame;
         private KeyboardHookListener m_KeyboardHookManager;
+        #endregion
+
+        #region Blackmagic ATEM
+        private IBMDSwitcherDiscovery m_switcherDiscovery;
+        private IBMDSwitcher m_switcher;
+        private IBMDSwitcherMixEffectBlock m_mixEffectBlock1;
+
+        private SwitcherMonitor m_switcherMonitor;
+        private MixEffectBlockMonitor m_mixEffectBlockMonitor;
+
+        private Boolean m_moveSliderDownwards = false;
+        private Boolean m_currentTransitionReachedHalfway = false;
+
+        private List<InputMonitor> m_inputMonitors = new List<InputMonitor>();
+
+        private long ProgramInputId;
+        private String ProgramInputName;
+        #endregion
 
         [DllImport("user32.dll")]
         static extern IntPtr GetForegroundWindow();
@@ -28,7 +50,28 @@ namespace OverlayDropper
         public OverlayDropper()
         {
             InitializeComponent();
+            ATEMIpAddressTextBox.Text = Properties.Settings.Default.IPAddress.ToString();
             m_KeyboardHookManager = new KeyboardHookListener(new GlobalHooker());
+
+            m_switcherMonitor = new SwitcherMonitor();
+            // note: this invoke pattern ensures our callback is called in the main thread. We are making double
+            // use of lambda expressions here to achieve this.
+            // Essentially, the events will arrive at the callback class (implemented by our monitor classes)
+            // on a separate thread. We must marshell these to the main thread, and we're doing this by calling
+            // invoke on the Windows Forms object. The lambda expression is just a simplification.
+            m_switcherMonitor.SwitcherDisconnected += new SwitcherEventHandler((s, a) => this.Invoke((Action)(() => SwitcherDisconnected())));
+
+            m_mixEffectBlockMonitor = new MixEffectBlockMonitor();
+            m_mixEffectBlockMonitor.ProgramInputChanged += new SwitcherEventHandler((s, a) => this.Invoke((Action)(() => UpdateItems())));
+            
+            m_switcherDiscovery = new CBMDSwitcherDiscovery();
+            if (m_switcherDiscovery == null)
+            {
+                MessageBox.Show("Could not create Switcher Discovery Instance.\nATEM Switcher Software may not be installed.", "Error");
+                Environment.Exit(1);
+            }
+
+            SwitcherDisconnected();		// start with switcher disconnected
         }
 
         #region Initialisation of events and application
@@ -46,6 +89,12 @@ namespace OverlayDropper
                 m_KeyboardHookManager.KeyUp -= HookManager_KeyUp;
             }
         }
+
+        private void ATEMIpAddressTextBox_TextChanged(object sender, EventArgs e)
+        {
+            Properties.Settings.Default.IPAddress = ATEMIpAddressTextBox.Text;
+            Properties.Settings.Default.Save();
+        }
         #endregion
 
         #region Event handlers of particular events. They will be activated when an appropriate checkbox is checked.
@@ -54,6 +103,19 @@ namespace OverlayDropper
         private void GameSelector_SelectedIndexChanged(object sender, EventArgs e)
         {
             SelectedGame = GameSelector.SelectedItem.ToString();
+
+            if (SelectedGame == "League of Legends")
+            {
+                HotKeyLabel.Text = "Toggle Overlay: A";
+            }
+            else if (SelectedGame == "Starcraft 2")
+            {
+                HotKeyLabel.Text = "Toggle Overlay: Ctrl+W";
+            }
+            else
+            {
+                HotKeyLabel.Text = "";
+            }
         }
         
         private void HookManager_KeyDown(object sender, KeyEventArgs e)
@@ -62,16 +124,16 @@ namespace OverlayDropper
             {
                 IsCtrlDown = true;
             }
-            if (e.KeyCode.ToString() == "LShiftKey")
+            else if (e.KeyCode.ToString() == "LShiftKey")
             {
                 IsShiftDown = true;
             }
-            if (e.KeyCode.ToString() == "LAltKey")
+            else if (e.KeyCode.ToString() == "LAltKey")
             {
                 IsAltDown = true;
             }
 
-            Log(string.Format("KeyDown \t\t {0}\n", e.KeyCode));
+            Log(string.Format("KeyDown \t {0}", e.KeyCode));
         }
 
         private void HookManager_KeyUp(object sender, KeyEventArgs e)
@@ -80,11 +142,11 @@ namespace OverlayDropper
             {
                 IsCtrlDown = false;
             }
-            if (e.KeyCode.ToString() == "LShiftKey")
+            else if (e.KeyCode.ToString() == "LShiftKey")
             {
                 IsShiftDown = false;
             }
-            if (e.KeyCode.ToString() == "LAltKey")
+            else if (e.KeyCode.ToString() == "LAltKey")
             {
                 IsAltDown = false;
             }
@@ -93,13 +155,12 @@ namespace OverlayDropper
             {
                 ToggleLeagueOfLegendsOverlays();
             }
-
-            if (SelectedGame == "Starcraft 2" && IsCtrlDown && e.KeyCode.ToString() == "W")
+            else if (SelectedGame == "Starcraft 2" && IsCtrlDown && e.KeyCode.ToString() == "W")
             {
                 ToggleStarcraft2Overlays();
             }
 
-            Log(string.Format("KeyUp  \t\t {0}\n", e.KeyCode));
+            Log(string.Format("KeyUp  \t {0}", e.KeyCode));
         }
         #endregion
 
@@ -134,6 +195,181 @@ namespace OverlayDropper
                 return Buff.ToString();
             }
             return null;
+        }
+
+        #region ATEM Connections
+        private void ATEMConnectButton_Click(object sender, EventArgs e)
+        {
+            _BMDSwitcherConnectToFailure failReason = 0;
+            string address = ATEMIpAddressTextBox.Text;
+
+            try
+            {
+                // Note that ConnectTo() can take several seconds to return, both for success or failure,
+                // depending upon hostname resolution and network response times, so it may be best to
+                // do this in a separate thread to prevent the main GUI thread blocking.
+                m_switcherDiscovery.ConnectTo(address, out m_switcher, out failReason);
+            }
+            catch (COMException)
+            {
+                // An exception will be thrown if ConnectTo fails. For more information, see failReason.
+                switch (failReason)
+                {
+                    case _BMDSwitcherConnectToFailure.bmdSwitcherConnectToFailureNoResponse:
+                        MessageBox.Show("No response from Switcher", "Error");
+                        break;
+                    case _BMDSwitcherConnectToFailure.bmdSwitcherConnectToFailureIncompatibleFirmware:
+                        MessageBox.Show("Switcher has incompatible firmware", "Error");
+                        break;
+                    default:
+                        MessageBox.Show("Connection failed for unknown reason", "Error");
+                        break;
+                }
+                return;
+            }
+
+            SwitcherConnected();
+        }
+
+        private void SwitcherConnected()
+        {
+            ATEMConnectButton.Enabled = false;
+
+            // Install SwitcherMonitor callbacks:
+            m_switcher.AddCallback(m_switcherMonitor);
+
+            // We create input monitors for each input. To do this we iterator over all inputs:
+            // This will allow us to update the combo boxes when input names change:
+            IBMDSwitcherInputIterator inputIterator;
+            if (SwitcherAPIHelper.CreateIterator(m_switcher, out inputIterator))
+            {
+                IBMDSwitcherInput input;
+                inputIterator.Next(out input);
+                while (input != null)
+                {
+                    InputMonitor newInputMonitor = new InputMonitor(input);
+                    input.AddCallback(newInputMonitor);
+                    newInputMonitor.LongNameChanged += new SwitcherEventHandler(OnInputLongNameChanged);
+
+                    m_inputMonitors.Add(newInputMonitor);
+
+                    inputIterator.Next(out input);
+                }
+            }
+
+            // We want to get the first Mix Effect block (ME 1). We create a ME iterator,
+            // and then get the first one:
+            m_mixEffectBlock1 = null;
+            IBMDSwitcherMixEffectBlockIterator meIterator;
+            SwitcherAPIHelper.CreateIterator(m_switcher, out meIterator);
+
+            if (meIterator != null)
+            {
+                meIterator.Next(out m_mixEffectBlock1);
+            }
+
+            if (m_mixEffectBlock1 == null)
+            {
+                MessageBox.Show("Unexpected: Could not get first mix effect block", "Error");
+                return;
+            }
+
+            // Install MixEffectBlockMonitor callbacks:
+            m_mixEffectBlock1.AddCallback(m_mixEffectBlockMonitor);
+            UpdateItems();
+        }
+
+        private void SwitcherDisconnected()
+        {
+            ATEMConnectButton.Enabled = true;
+
+            // Remove all input monitors, remove callbacks
+            foreach (InputMonitor inputMon in m_inputMonitors)
+            {
+                inputMon.Input.RemoveCallback(inputMon);
+                inputMon.LongNameChanged -= new SwitcherEventHandler(OnInputLongNameChanged);
+            }
+            m_inputMonitors.Clear();
+
+            if (m_mixEffectBlock1 != null)
+            {
+                // Remove callback
+                m_mixEffectBlock1.RemoveCallback(m_mixEffectBlockMonitor);
+
+                // Release reference
+                m_mixEffectBlock1 = null;
+            }
+
+            if (m_switcher != null)
+            {
+                // Remove callback:
+                m_switcher.RemoveCallback(m_switcherMonitor);
+
+                // release reference:
+                m_switcher = null;
+            }
+        }
+
+
+        private void UpdateItems()
+        {
+            m_mixEffectBlock1.GetInt(_BMDSwitcherMixEffectBlockPropertyId.bmdSwitcherMixEffectBlockPropertyIdProgramInput, out ProgramInputId);
+
+            // Get an input iterator. We use the SwitcherAPIHelper to create the iterator for us:
+            IBMDSwitcherInputIterator inputIterator;
+            if (!SwitcherAPIHelper.CreateIterator(m_switcher, out inputIterator))
+                return;
+
+            IBMDSwitcherInput input;
+            inputIterator.Next(out input);
+            while (input != null)
+            {
+                string inputName;
+                long inputId;
+
+                input.GetInputId(out inputId);
+                input.GetString(_BMDSwitcherInputPropertyId.bmdSwitcherInputPropertyIdLongName, out inputName);
+
+                if (inputId == ProgramInputId)
+                {
+                    ProgramInputName = inputName;
+                }
+            }
+
+            if (ProgramInputName != "")
+            {
+                ProgLabel.Text = string.Format("Prog: {0}", ProgramInputName);
+            }
+            else
+            {
+                ProgLabel.Text = "";
+            }
+        }
+
+        private void OnInputLongNameChanged(object sender, object args)
+        {
+            this.Invoke((Action)(() => UpdateItems()));
+        }
+        #endregion
+
+        /// <summary>
+        /// Used for putting other object types into combo boxes.
+        /// </summary>
+        struct StringObjectPair<T>
+        {
+            public string name;
+            public T value;
+
+            public StringObjectPair(string name, T value)
+            {
+                this.name = name;
+                this.value = value;
+            }
+
+            public override string ToString()
+            {
+                return name;
+            }
         }
     }
 }
